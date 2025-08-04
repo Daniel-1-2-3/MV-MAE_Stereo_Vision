0f3f4a9d20c1a0cd914aa304422323a6d4bf51e9
Daniel-1-2-3 <144050857+Daniel-1-2-3@users.noreply.github.com>
2025-08-04 01:50:06 -0700

    log reward

--- SawyerSim/sac.py
+++ SawyerSim/sac.py
@@ -146,7 +146,7 @@ class Agent():
         self.critic_1.optimizer.step()
         self.critic_2.optimizer.step()
         
-        print('MVMAE_Loss:', mvmae_loss.item(), '\t', 'Policy_Loss:', policy_loss.item(), '\t', 'Reward:', reward_float)
+        print('MVMAE_Loss:', mvmae_loss.item(), '\t', 'Policy_Loss:', policy_loss.item(), '\t', 'Reward:', sum(list(reward_float)) / len(list(reward_float)))
         with open(log_file, mode='a', newline='') as f:
             writer = csv.writer(f)
             writer.writerow([mvmae_loss.item(), policy_loss.item()])
