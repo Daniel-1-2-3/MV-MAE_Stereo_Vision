3b4e159bb1878ff8153f645eab506eaf53290431
Daniel-1-2-3 <144050857+Daniel-1-2-3@users.noreply.github.com>
2025-08-06 21:11:26 -0700

    opengl

--- trainer_pipeline.py
+++ trainer_pipeline.py
@@ -1,10 +1,10 @@
-# import os
-# os.environ["MUJOCO_GL"] = "egl"
+import os
+os.environ["MUJOCO_GL"] = "egl"
 
 from SawyerSim.sawyer_stereo_env import SawyerReachEnvV3
 from SawyerSim.custom_sac import SAC
 from SawyerSim.custom_sac_policy import SACPolicy
 
 env = SawyerReachEnvV3(render_mode="human")
-model = SAC(SACPolicy, env, buffer_size=3000, verbose=1)
+model = SAC(SACPolicy, env, buffer_size=500, verbose=1)
 model.learn(total_timesteps=10_000, log_interval=4, progress_bar=True)
