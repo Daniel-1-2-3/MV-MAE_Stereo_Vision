01510e8ff3287c969d00746575758b1f45b676e3
Daniel-1-2-3 <144050857+Daniel-1-2-3@users.noreply.github.com>
2025-08-07 18:06:27 -0700

    batch size, requirements.txt

--- trainer_pipeline.py
+++ trainer_pipeline.py
@@ -1,10 +1,11 @@
-import os
-os.environ["MUJOCO_GL"] = "osmesa"
+# Only when running in RunPod hosted GPU, not local environment
+# import os
+# os.environ["MUJOCO_GL"] = "osmesa"
 
 from SawyerSim.sawyer_stereo_env import SawyerReachEnvV3
 from SawyerSim.custom_sac import SAC
 from SawyerSim.custom_sac_policy import SACPolicy
 
 env = SawyerReachEnvV3(render_mode="rgb_array")
-model = SAC(SACPolicy, env, buffer_size=3000, verbose=1, batch_size=32)
+model = SAC(SACPolicy, env, buffer_size=1_000_000, verbose=1, batch_size=32, learning_starts=50_000)
 model.learn(total_timesteps=10_000, log_interval=4, progress_bar=True)
