309b5c50648c58bfc3ce02b9af79bc8dea18d907
Daniel-1-2-3 <144050857+Daniel-1-2-3@users.noreply.github.com>
2025-12-20 22:40:03 -0500

    jax versioning

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from absl import app
 from train_drqv2_mujoco import Workshop, get_args, save_agent
 from Custom_Mujoco_Playground.learning.train_jax_ppo import main
 
+import jax
+
+if not hasattr(jax.sharding, "AxisType"):
+    class AxisType(str):
+        pass
+    jax.sharding.AxisType = AxisType
+
 def main():
     args = get_args()
     app.run(main)
