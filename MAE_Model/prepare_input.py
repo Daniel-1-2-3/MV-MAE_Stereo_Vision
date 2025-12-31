26d58232f728e91dbb80f49996e738f24a7afa2f
Daniel-1-2-3 <144050857+Daniel-1-2-3@users.noreply.github.com>
2025-12-30 22:43:13 -0500

    case to float

--- MAE_Model/prepare_input.py
+++ MAE_Model/prepare_input.py
@@ -1,22 +1,25 @@
 import torch
 
 class Prepare:
-    _cached = {}  # (device, dtype) -> (mean, std)
+    _cached = {}  # device -> (mean_f32, std_f32)
 
     @staticmethod
-    def _stats(device, dtype):
-        key = (device, dtype)
-        if key not in Prepare._cached:
-            mean = torch.tensor([0.51905, 0.47986, 0.48809], device=device, dtype=dtype).view(1, 1, 1, 3)
-            std  = torch.tensor([0.17454, 0.20183, 0.19598], device=device, dtype=dtype).view(1, 1, 1, 3)
-            Prepare._cached[key] = (mean, std)
-        return Prepare._cached[key]
+    def _stats(device):
+        if device not in Prepare._cached:
+            mean = torch.tensor([0.51905, 0.47986, 0.48809], device=device, dtype=torch.float32).view(1,1,1,3)
+            std  = torch.tensor([0.17454, 0.20183, 0.19598], device=device, dtype=torch.float32).view(1,1,1,3)
+            Prepare._cached[device] = (mean, std)
+        return Prepare._cached[device]
 
     @staticmethod
     def normalize(x: torch.Tensor) -> torch.Tensor:
-        # If x came from a view / custom source, make it safe
         if x.is_cuda and not x.is_contiguous():
             x = x.contiguous()
-        x = x.to(torch.float32)
-        mean, std = Prepare._stats(x.device, x.dtype)
-        return (x - mean) / std
+            
+        if x.dtype == torch.uint8:
+            x = x.to(torch.float32) * (1.0 / 255.0)
+        else:
+            x = x.to(torch.float32)
+
+        mean, std = Prepare._stats(x.device)
+        return (x - mean) / st
--- Mujoco_Sim/brax_wrapper.py
+++ Mujoco_Sim/brax_wrapper.py
@@ -38,15 +38,11 @@ except ImportError:
     TensorDict = None
 
 def _jax_to_torch(tensor):
-    import torch.utils.dlpack as tpack  # pytype: disable=import-error # pylint: disable=import-outside-toplevel
-    tensor = tpack.from_dlpack(tensor)
-    return tensor
-
-def _torch_to_jax(tensor):
-    from jax.dlpack import from_dlpack  # pylint: disable=import-outside-toplevel
-
-    tensor = from_dlpack(tensor)
-    return tensor
+    import torch.utils.dlpack as tpack
+    # Make sure JAX finished producing the buffer
+    if hasattr(tensor, "block_until_ready"):
+        tensor.block_until_ready()
+    return tpack.from_dlpack(tensor)
 
 def get_load_path(root, load_run=-1, checkpoint=-1):
     try:
